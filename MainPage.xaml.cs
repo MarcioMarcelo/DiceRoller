39b96a3df8f12996542a1ba1a88eac1e7554039f
Marcelo e Marcio <etecmarciomarcelo@gmail.com>
2023-09-01 22:57:53 -0300

    Primeiro Commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void rollDice(object sender, EventArgs e)
+	{
+		
+	}
+}
+
